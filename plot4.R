d15ffbe767284c54f8bbe2f88eb777b59a7efa4c
mtabares <33518048+mtabares@users.noreply.github.com>
2018-02-18 22:05:39 -0500

    Add files via upload

--- plot4.R
+++ plot4.R
@@ -31,16 +31,17 @@ par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
 with(data2plot, {
   plot(Global_active_power~dt2plot, type="l", 
        ylab="Global Active Power (kilowatts)", xlab="")
-  plot(Voltage~dt2plot, type="l", 
-       ylab="Voltage (volt)", xlab="")
-  plot(Sub_metering_1~dt2plot, type="l", 
-       ylab="Energy sub metering", xlab="")
+  plot(Voltage~dt2plot, type="l", ylab="Voltage (volt)", xlab="")
+  plot(Sub_metering_1~dt2plot, type="l", ylab="Energy sub metering", xlab="")
+  
   lines(Sub_metering_2~dt2plot,col='Red')
   lines(Sub_metering_3~dt2plot,col='Blue')
-  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
-         legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-  plot(Global_reactive_power~dt2plot, type="l", 
-       ylab="Global_Rective_Power",xlab="")
+
+  legend("topright", lty=1, col=c("black","red","blue"),
+         legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), cex = 0.3)
+  
+
+  plot(Global_reactive_power~dt2plot, type="l", ylab="Global_Rective_Power",xlab="")
 })
 ## Saving to file
 dev.copy(png, file="plot4.png", height=480, width=480)
